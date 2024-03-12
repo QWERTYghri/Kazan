2888504743e68eff6dd0a7680bca9eade0442634
QWERTYbae <nitwitghfj17717@protonmail.com>
2024-03-12 12:54:00 -0700

    Updated main.c

---
+++ src/define.h
@@ -0,0 +1,9 @@
+/*
+ * List of Definitions
+ *
+ *
+ */
+ 
+#define ERROR_NO_OPTIONS ( "Error : Non-Option Arguments\n\n" )
+#define GET_OPT_ERROR ( "Error : Getopt returned code [%d]\n\n" )
+#define INVALID_COMMAND ( "Incorrect : [%s]\n )
--- src/main.c
+++ src/main.c
@@ -12,13 +12,58 @@
 #include <unistd.h>
 #include <getopt.h>
 
+/* Local */
+#include "./define.h"
+
+/* Parse Options Functions */
 static int32_t
+parseOptions ( int32_t argc, char** argv )
+{
+	/* Long Argument List */
+	struct option commandOptions[] =
+	{
+		{ "clean",	no_argument,	   NULL, 'c' },
+		{ "add",	required_argument, NULL, 'a' },
+		{ NULL,		0,		   NULL, 0 }
+	};
+	
+	/* Parse arguments and return argument code */
+	int32_t character;
+	int optionIndex = 0;
+	while ( ( character = getopt_long ( argc, argv, "ca:", commandOptions, &optionIndex ) ) != -1 )
+	{
+		switch ( character )
+		{
+			case 'c':
+				return 1;
+			case 'a':
+				return 2;
+			default:
+				fprintf ( stderr, GET_OPT_ERROR, character );
+				return -1;
+		}
+	}
 
+	/* Arguments Check */	
+	if ( optind < argc )
+	{
+		/* List that the options are invalid */
+		fprintf ( stderr, ERROR_NO_OPTIONS );
+		
+		/* List invalid commands written */
+		while ( optind < argc )
+			fprintf ( stderr, INVALID_COMMAND, argv[optind++] );
+	}
+	
+	return -1;
+}
 
+/* Start Point */
 int
 main ( int argc, char** argv )
 {
 	int32_t result = parseOptions ( argc, argv );
+	printf ( "%d\n", result );
 
 	return 0;
 }
