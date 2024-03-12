ad29c531dca01ce73cfb7335c2b34b47ce7b5790
QWERTYbae <nitwitghfj17717@protonmail.com>
2024-03-12 00:00:56 -0700

    Changed main.c

--- src/main.c
+++ src/main.c
@@ -5,15 +5,20 @@
  
 /* LIB C */
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
 /* POSIX */
+#include <unistd.h>
 #include <getopt.h>
+
+static int32_t
 
 
 int
-main ( void )
+main ( int argc, char** argv )
 {
-	printf ( "test" );
+	int32_t result = parseOptions ( argc, argv );
 
 	return 0;
 }
